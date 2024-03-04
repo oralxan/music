1d9d0e3629dfb447f68714799989439589f5d717
oralxan <oralxan@users.noreply.github.com>
2024-03-05 01:26:09 +0500

    settings

--- src/settings/base.py
+++ src/settings/base.py
@@ -29,8 +29,12 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAdminUser'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
 
 
 MIDDLEWARE = [
