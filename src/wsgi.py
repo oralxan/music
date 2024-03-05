2516306152fe84864ffec285f430a0f6901847dc
oralxan <oralxan@users.noreply.github.com>
2024-03-05 23:25:39 +0500

    f

--- src/wsgi.py
+++ src/wsgi.py
@@ -7,4 +7,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings.production')
 
 application = get_wsgi_application()
-app = application
