f34b8a055d5dbf0cc3169e7f1947e2932a0a1ff0
oralxan <oralxan@users.noreply.github.com>
2024-03-09 21:01:46 +0500

    m

--- src/settings/production.py
+++ src/settings/production.py
@@ -6,10 +6,8 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('PRODUCTION_DB_NAME'),
-        'USER': config('PRODUCTION_DB_USER'),
-        'PASSWORD': config('PRODUCTION_DB_PASSWORD'),
-        'HOST': config('PRODUCTION_DB_HOST')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'production.sqlite3',
     }
 }
+
